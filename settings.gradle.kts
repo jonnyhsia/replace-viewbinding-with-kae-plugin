eca3d6030d75a32bc04d2fa839441b050aadf147
GitHub Action <action@github.com>
2022-03-31 03:12:07 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "replace-viewbinding-with-kae-plugin"
---
+++ src/main/kotlin/com/github/jonnyhsia/replaceviewbindingwithkaeplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jonnyhsia.replaceviewbindingwithkaeplugin.services
+
+import com.github.jonnyhsia.replaceviewbindingwithkaeplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
